fc9671fea61254755007261d34fe909c304d60cc
Ana Paixão <aninha.beel@gmail.com>
2022-01-07 09:48:55 -0300

    last name added in users

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -4,22 +4,22 @@ const dummyUsers = {
   '1': User(
       id: "1",
       cpf: '1827364758',
-      userName: 'Maria',
-      mothersName: 'Antonia',
+      userName: 'Maria Ferreira',
+      mothersName: 'Antonia Ferreira',
       birth: '12/03/1976',
       gender: 'feminino'),
   '2': User(
       id: '2',
       cpf: '26374876281',
-      userName: 'Rafael',
-      mothersName: 'Joyce',
+      userName: 'Rafael Oliveira',
+      mothersName: 'Joyce Oliveira',
       birth: '30/12/1876',
       gender: 'masculino'),
   '3': User(
       id: '3',
       cpf: '26981076281',
-      userName: 'Fernanda',
-      mothersName: 'Camilla',
+      userName: 'Fernanda Lima',
+      mothersName: 'Camilla Lima',
       birth: '13/05/1892',
       gender: 'feminino')
 };
